6adf7b2a81e762d4b02ac9ced18f2a129a1461f8
Yoon Young <yunyoung1819@hanmail.net>
2017-11-19 12:54:14 +0900

    Java_dailyCoding_200.git

---
+++ src/main/java/coding_1_50/Hello.java
@@ -0,0 +1,12 @@
+package coding_1_50;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		
+		String str = "Hello! This is Java Study Project!";
+		
+		System.out.println(str);
+	}
+
+}
