753d6ced987578e51bb38894e526bc3703e67e9b
Yoon Young <yunyoung1819@hanmail.net>
2017-11-21 23:58:49 +0900

    zzz dddddddddddsdsdsds

---
+++ src/main/java/coding_1_50/Hello4.java
@@ -0,0 +1,11 @@
+package coding_1_50;
+
+public class Hello4 {
+	
+	public static void main(String[] args){
+		
+		String str = "Hello Wolrd! This is Java Study Project!!";
+		
+		System.out.println(str);
+	}
+}
