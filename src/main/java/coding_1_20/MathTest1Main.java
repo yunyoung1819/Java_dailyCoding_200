16e9d5831b3d200b1aacd19940ac01115d0dd78d
yunyoung1819 <yunyoung1819@hanmail.net>
2018-10-22 03:14:08 +0900

    15번 java.lang.Math 실습 코드 추가

--- src/main/java/coding_1_20/MathTest1Main.java
+++ src/main/java/coding_1_20/MathTest1Main.java
@@ -8,34 +8,33 @@ public class MathTest1Main {
 	public static void main(String[] args) {
 		
 		// constant
-		System.out.println("1: Math.E" + Math.E);    // 자연로그 E
-		System.out.println("2: Math.PI" + Math.PI);  // 원주율 PI
+		System.out.println("1: Math.E" + Math.E);	// 자연로그 E
+		System.out.println("2: Math.PI" + Math.PI);	// 원주율 PI
 		
 		// method
-		System.out.println("3: Math.abs(-5) 절대값:" + Math.abs(-5));          // 절대값
-	    System.out.println("4: Math.ceil(4.34) 올림:" + Math.ceil(4.34));     // 올림
-	    System.out.println("5: Math.round(4.34) 반올림:" + Math.round(4.34));  // 반올림
-	    System.out.println("6: Math.floor(4.34) 버림:" + Math.floor(4.34));    // 버림
-	    System.out.println("7: Math.rint(4.34) 반올림:" + Math.rint(4.34));     //반올림
+		System.out.println("3: Math.abs(-5) 절대값:" + Math.abs(-5));	// 절대값
+	    System.out.println("4: Math.ceil(4.34) 올림:" + Math.ceil(4.34));	// 올림
+	    System.out.println("5: Math.round(4.34) 반올림:" + Math.round(4.34));	// 반올림
+	    System.out.println("6: Math.floor(4.34) 버림:" + Math.floor(4.34));	// 버림
+	    System.out.println("7: Math.rint(4.34) 반올림:" + Math.rint(4.34));	//반올림
 	    
-	    System.out.println("8: Math.max(45,78)" + Math.max(45, 78));          // 최대값
-	    System.out.println("9: Math.min(45,78)" + Math.min(45, 78));          // 최소값
+	    System.out.println("8: Math.max(45,78)" + Math.max(45, 78));	// 최대값
+	    System.out.println("9: Math.min(45,78)" + Math.min(45, 78));	// 최소값
 	    
-	    System.out.println("10: Math.pow(2,4)" + Math.pow(2, 4));			  // 승수 pow(2,4)는 2의 4승
-	    System.out.println("11: Math.log(30)" + Math.log(30));				  // 밑수가 Math.E인 로그
-	    System.out.println("12: Math.exp(3)" + Math.exp(3));				  // exp는 자연 지수의 승수
-	    System.out.println("13: Math.sqrt(10)" + Math.sqrt(10));              // 양의 제곱근
+	    System.out.println("10: Math.pow(2,4)" + Math.pow(2, 4));	// 승수 pow(2,4)는 2의 4승
+	    System.out.println("11: Math.log(30)" + Math.log(30));	// 밑수가 Math.E인 로그
+	    System.out.println("12: Math.exp(3)" + Math.exp(3));	// exp는 자연 지수의 승수
+	    System.out.println("13: Math.sqrt(10)" + Math.sqrt(10));	// 양의 제곱근
 	   
-	    System.out.println("14: Math.sin(Math.toRadians(90))");               // 도(degree)를 라디안(radian)으로 바꾼다
-	    System.out.println(Math.sin(Math.toRadians(90)));                     // 라디안을 sin 삼각함수에 입력한다. 삼각함수의 입력값은 라디안으로 바꿔야한다
+	    System.out.println("14: Math.sin(Math.toRadians(90))");	// 도(degree)를 라디안(radian)으로 바꾼다
+	    System.out.println(Math.sin(Math.toRadians(90)));	// 라디안을 sin 삼각함수에 입력한다. 삼각함수의 입력값은 라디안으로 바꿔야한다
 	    
 	    System.out.println("15: Math.cos(Math.toRadians(180))");             
-	    System.out.println(Math.cos(Math.toRadians(180)));                    // cos 삼각함수
+	    System.out.println(Math.cos(Math.toRadians(180)));	// cos 삼각함수
 	    
 	    // random
-	    
-	    double dRan = Math.random(); // 0 <== dRan < 1 실수
-	    int iRan = (int)(Math.random()*100);   // 0 <== iRan <100 정수
+	    double dRan = Math.random();	// 0 <== dRan < 1 실수
+	    int iRan = (int)(Math.random()*100);	// 0 <== iRan <100 정수
 	    System.out.println("16: Math.random() => " + dRan);
 	    System.out.println("17: (int)(Math.random()*100) => " + iRan);
 	} 
--- src/main/java/coding_1_20/MathTest2Main.java
+++ src/main/java/coding_1_20/MathTest2Main.java
@@ -1,8 +1,31 @@
 package coding_1_20;
 
+import static java.lang.Math.*;
+
 public class MathTest2Main {
 	
 	public static void main(String[] args) {
 		
+		// 추가 메서드
+		System.out.printf("1: log10(100) %-20.10f %n", Math.log10(100)); 	// 상용로그
+	    System.out.printf("2: sinh(3) %-20.10f%n", sinh(3));				// hyperbolic sin (쌍곡사인)
+	    System.out.printf("3: 모드1 %-10.5f%n", IEEEremainder(10.21,3.34));	// double 타입의 %(모드) 연산. %-10.5f와 %f는 실수 출력
+	    System.out.printf("4: 모드2 %-10.5f%n", 10.21%3.34);					// double % 연산
+	    System.out.printf("5: 모드3 %-10.5f%n", (10.21-3.34*((int)(10.21/3.34))));  // double % 연산을 (int) 캐스팅을 이용하여 구현
+	    
+	    // 소수 원하는 곳에서 반올림하기 
+	    double d = 10.34567;
+	    System.out.printf("6: 원하는 곳에서 반올림하기 %n");
+	    System.out.printf("7: 1: %10.6f %10.6f %n", d, olimAtNums(d, 1));
+	    System.out.printf("7: 2: %10.6f %10.6f %n", d, olimAtNums(d, 2));
+	    System.out.printf("7: 3: %10.6f %10.6f %n", d, olimAtNums(d, 3));
+	    System.out.printf("7: 4: %10.6f %10.6f %n", d, olimAtNums(d, 4));
+	    System.out.printf("7: 5: %10.6f %10.6f %n", d, olimAtNums(d, 5));
 	}
+
+	public static double olimAtNums(double d, int nums) {
+		
+		return ((int)((d * Math.pow(10, nums)+5)/10.0))/Math.pow(10, nums-1);
+	} // olimAtNums
+	
 }
