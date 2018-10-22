4cb044024115f5cb261abaa05c811ad1abcee931
yunyoung1819 <yunyoung1819@hanmail.net>
2018-10-22 13:21:48 +0900

    16번 참조타입 String 코드 추가

---
+++ src/main/java/coding_1_20/StringTest1Main.java
@@ -0,0 +1,54 @@
+package coding_1_20;
+
+/**
+ * 16. 참조 타입 String
+ * 
+ */
+public class StringTest1Main {
+	public static void main(String[] args) {
+		//String Concatenation
+		String sname = "";
+		sname += "Antony ";
+		sname += "Sedas ";
+		sname += 6 + "th";
+		
+		// String Substring
+		System.out.println("1:    01234567890123456789");
+		System.out.println("2: Name :" + sname);
+		
+		sname = sname.substring(2);
+		System.out.println("3: substring: " + sname);
+		
+		sname = sname.substring(2,5);
+		System.out.println("4: substring: " + sname);
+		
+		// pass by value
+		String sChange = "hyonny%love";
+		System.out.print("5: main: Before: ");
+		System.out.println("Hot Live : " + sChange);
+		
+	    changeIn(sChange);
+	    System.out.print("7: main: After: ");
+	    System.out.println("Hot Live : " + sChange);
+	    
+	    System.out.print("8: ,<>/%^&*+-=() 이 있다면 true : ");
+	    System.out.println(hasDelemeter('<'));
+	}
+
+	// ,<>/%^&*+-=() 문자가 있다면 true
+	public static boolean hasDelemeter(char c) {
+		boolean hasD = false;
+		if(",<>/%^&*+-=()".indexOf(c) != -1) {
+			hasD = true;
+		}
+		return hasD;
+	}
+
+	// pass by value test
+	public static void changeIn(String str) {
+		str = str.replace('o', 'z');
+		str += "hello";
+		System.out.print("6: method changeIn: ");
+		System.out.println("Heart " + str);
+	}
+}
